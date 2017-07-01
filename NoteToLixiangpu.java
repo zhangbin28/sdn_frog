2a1287653780dfc9d7310fdc03aea00ea60b45eb
28 <fealie28@163.com>
2017-07-01 20:33:12 +0800

    add NoteToLixiangpu.java

---
+++ NoteToLixiangpu.java
@@ -0,0 +1,6 @@
+//7.1
+function need finish in module: net.floodlightcontroll.bandwidth
+void changeTounQueue(int,String)
+int createQueue(double,double)   return queueId that qos create
+void changeQueue(int.double,double)
+//7.1
